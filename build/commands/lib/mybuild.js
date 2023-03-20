c5e58a0319360bb2c2aaf08e0f2c260ce4656656
DESKTOP-8S37CJN\TraNguyen <66865850+nstra9x@users.noreply.github.com>
2023-03-20 13:46:41 +0700

    Update continue 14

---
+++ build/commands/lib/mybuild.js
@@ -0,0 +1,16 @@
+const config = require('../lib/config')
+const util = require('../lib/util')
+const path = require('path')
+const fs = require('fs-extra')
+const { log } = require('console')
+
+console.log('Building project...')
+util.run('which', ['autoninja'], { cwd: config.srcDir })
+let ninjaOpts = [
+    '-C',
+    'out/Debug-iphonesimulator',
+    'gn_all'
+]
+console.log('Building..................')
+util.run('autoninja', ninjaOpts, { cwd: config.srcDir })
+console.log('Finish building project')
