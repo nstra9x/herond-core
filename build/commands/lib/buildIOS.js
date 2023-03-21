33ad2437bc547e2b4e07e9c30805d909b681b0dc
DESKTOP-8S37CJN\TraNguyen <66865850+nstra9x@users.noreply.github.com>
2023-03-21 13:35:25 +0700

    Update 22

--- build/commands/lib/buildIOS.js
+++ build/commands/lib/buildIOS.js
@@ -3,9 +3,51 @@ const util = require('../lib/util')
 const path = require('path')
 const fs = require('fs-extra')
 
-const buildIOS = () => {
+const buildIOS = (buildConfig = config.defaultBuildConfig, options) => {
+  config.buildConfig = buildConfig
+  config.update(options)
+
+  switch (options.build_mode.toLowerCase()) {
+    case "debug":
+      options.build_mode = "Debug"
+      break
+    case "official":
+      options.build_mode = "Official"
+      break
+    case "profile":
+      options.build_mode = "Profile"
+      break
+    default:
+      options.build_mode = "Release"
+  }
+
+  switch (options.build_option.toLowerCase()) {
+    case "content_shell":
+      options.build_option = "content_shell"
+      break
+    case "ios_web_shell":
+      options.build_option = "ios_web_shell"
+      break
+    default:
+      options.build_option = "gn_all"
+  }
+
+  switch (options.build_device.toLowerCase()) {
+    case "iphone":
+      options.build_device = "iphoneos"
+      break
+    case "maccatalyst":
+      options.build_device = "maccatalyst"
+      break
+    default:
+      options.build_device = "iphonesimulator"
+  }
+
+  const outDir = path.resolve(path.join('out', options.build_mode, '-', options.build_device))
+  console.log('outDir == ' + outDir)
+
   console.log("Building project ...")
-  util.run('autoninja', ['-C', 'out/Debug-iphonesimulator', 'gn_all'], { stdio: 'inherit' }, cwd: config.srcDir)
+  util.run('autoninja', ['-C', outDir, options.build_option], { stdio: 'inherit', cwd: config.srcDir })
   console.log("... build project done")
 }
 
